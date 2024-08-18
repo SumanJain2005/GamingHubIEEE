8104d05c36b466f300155624f44b4510d81bb754
Ankan Misra <ankanmisra@Ankans-MacBook-Air.local>
2024-08-18 16:25:06 +0530

    ImageSlider Update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import Navbar from './components/navbar/navbar.jsx';
 import StartingHeading from './components/StartingHeading/StartingHeading.jsx';
+import ImageSlider from './components/ImageSlider/ImageSlider.jsx';
 
+function App() {
+  const slides = [
+      { image: './images/image1.png' },
+      { image: './images/image2.png' },
+      { image: './images/image4.png' }
+      // Add more slides as needed
+  ];
 
-const App = () => {
   return (
     <div>
       <Navbar />
       <StartingHeading />
-      {/* Add the SlidingPanel component here */}
+      <ImageSlider slides={slides} />
       {/* Add other components such as the slider menu here */}
     </div>
   );
-};
+}
 
 export default App;
