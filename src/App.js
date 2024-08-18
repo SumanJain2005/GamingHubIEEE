5dd60823e4deae56263347a9be2e7452982e704e
SumanJain2005 <sumannareshclg@gmail.com>
2024-08-18 19:32:03 +0530

    Background optimized

--- src/App.js
+++ src/App.js
@@ -5,22 +5,21 @@ import ImageSlider from './components/ImageSlider/ImageSlider.jsx';
 import Starting from './components/Starting/Starting.jsx';
 import About from './components/About/About.jsx';
 import Footer from './components/Footer/Footer.jsx';
-import Squares from './components/Squares/Squares.jsx';  // Importing the Squares component
+import Squares from './components/Squares/Squares.jsx';  
 
 function App() {
   const slides = [
       { image: './images/Image1.png' },
       { image: './images/Image2.png' },
-      { image: './images/Image3.png' },
-      { image: './images/Image4.png' }, // Added a fourth slide
+      { image: './images/Image3.png' }, 
   ];
 
   return (
     <div className="App">
       <Squares 
         speed={0.5} 
-        size={40} // Size of squares in pixels
-        direction='diagonal' // Movement direction of squares
+        size={40} 
+        direction='diagonal' 
         borderColor='#fff'
         hoverFillColor='#222'
       />
