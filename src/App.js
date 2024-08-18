2857dc1b172f572c39f4c5bb33281ea99c1516a5
SumanJain2005 <sumannareshclg@gmail.com>
2024-08-18 18:36:32 +0530

    Hello

--- src/App.js
+++ src/App.js
@@ -5,24 +5,31 @@ import ImageSlider from './components/ImageSlider/ImageSlider.jsx';
 import Starting from './components/Starting/Starting.jsx';
 import About from './components/About/About.jsx';
 import Footer from './components/Footer/Footer.jsx';
+import Squares from './components/Squares/Squares.jsx';  // Added the Squares component
 
 function App() {
   const slides = [
       { image: './images/Image1.png' },
       { image: './images/Image2.png' },
       { image: './images/Image3.png' },
-      // Add more slides as needed
+      { image: './images/Image4.png' }, // Added a fourth slide
   ];
 
   return (
     <div>
+      <Squares 
+        speed={0.5} 
+        size={40} // pixels
+        direction='diagonal' // up, down, left, right, diagonal
+        borderColor='#fff'
+        hoverFillColor='#222'
+      />
       <Navbar />
       <StartingHeading />
       <ImageSlider slides={slides} />
       <Starting />
       <About />
-      <Starting /> {/* Reuse the Starting component */}
-      
+      <Starting /> {/* Reusing the Starting component */}
       <Footer />
     </div>
   );
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,79 +3,83 @@ import './Footer.css';
 
 const Footer = () => {
     return (
-        <footer className="footer-section bg-dark text-white py-4">
-            <div className="footer-divider">
-                <div className="footer-left">
-                    <img 
-                        src="./images/IEEELogo.png" 
-                        alt="IEEE Logo" 
-                        className="footer-logo" 
-                    />
+        <>
+            {/* Fluorescent line above the footer */}
+            <div className="fluorescent-line"></div> {/* Line above the footer */}
+            <footer className="footer-section bg-dark text-white py-4">
+                <div className="footer-divider">
+                    <div className="footer-left">
+                        <img 
+                            src="./images/IEEELogo.png" 
+                            alt="IEEE Logo" 
+                            className="footer-logo" 
+                        />
+                    </div>
+                    <div className="footer-center">
+                        <a 
+                            href="https://wa.me/" 
+                            className="text-white me-3"
+                        >
+                            <i className="fab fa-whatsapp fa-2x"></i>
+                        </a>
+                        <a 
+                            href="https://linkedin.com" 
+                            className="text-white me-3"
+                        >
+                            <i className="fab fa-linkedin fa-2x"></i>
+                        </a>
+                        <a 
+                            href="https://facebook.com" 
+                            className="text-white me-3"
+                        >
+                            <i className="fab fa-facebook fa-2x"></i>
+                        </a>
+                        <a 
+                            href="https://instagram.com" 
+                            className="text-white me-3"
+                        >
+                            <i className="fab fa-instagram fa-2x"></i>
+                        </a>
+                    </div>
+                    <div className="footer-right">
+                        <img 
+                            src="./images/VortexLogo.png" 
+                            alt="Stylized Graphic" 
+                            className="footer-logo" 
+                        />
+                    </div>
                 </div>
-                <div className="footer-center">
+                <div className="footer-links">
                     <a 
-                        href="https://wa.me/" 
-                        className="text-white me-3"
+                        href="#about" 
+                        className="text-white"
                     >
-                        <i className="fab fa-whatsapp fa-2x"></i>
+                        Lorem ipsum
                     </a>
                     <a 
-                        href="https://linkedin.com" 
-                        className="text-white me-3"
+                        href="#contact" 
+                        className="text-white"
                     >
-                        <i className="fab fa-linkedin fa-2x"></i>
+                        consectetur
                     </a>
                     <a 
-                        href="https://facebook.com" 
-                        className="text-white me-3"
+                        href="#terms" 
+                        className="text-white"
                     >
-                        <i className="fab fa-facebook fa-2x"></i>
+                        Lorem ipsum
                     </a>
                     <a 
-                        href="https://instagram.com" 
-                        className="text-white me-3"
+                        href="#privacy" 
+                        className="text-white"
                     >
-                        <i className="fab fa-instagram fa-2x"></i>
+                        consectetur
                     </a>
                 </div>
-                <div className="footer-right">
-                    <img 
-                        src="./images/VortexLogo.png" 
-                        alt="Stylized Graphic" 
-                        className="footer-logo" 
-                    />
+                <div className="footer-bottom mt-4">
+                    <p className="text-center">Lorem ipsumipsum</p>
                 </div>
-            </div>
-            <div className="footer-links">
-                <a 
-                    href="#about" 
-                    className="text-white"
-                >
-                    Lorem ipsum
-                </a>
-                <a 
-                    href="#contact" 
-                    className="text-white"
-                >
-                    consectetur
-                </a>
-                <a 
-                    href="#terms" 
-                    className="text-white"
-                >
-                    Lorem ipsum
-                </a>
-                <a 
-                    href="#privacy" 
-                    className="text-white"
-                >
-                    consectetur
-                </a>
-            </div>
-            <div className="footer-bottom mt-4">
-                <p className="text-center">IEEE SB AOT</p>
-            </div>
-        </footer>
+            </footer>
+        </>
     );
 };
 
