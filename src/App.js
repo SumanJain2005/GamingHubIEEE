b467a2dac72423ede108441dee5a3facd4a32a83
Ankan Misra <ankanmisra@Ankans-MacBook-Air.local>
2024-08-18 18:43:02 +0530

    Fixed moving bG

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ImageSlider from './components/ImageSlider/ImageSlider.jsx';
 import Starting from './components/Starting/Starting.jsx';
 import About from './components/About/About.jsx';
 import Footer from './components/Footer/Footer.jsx';
-import Squares from './components/Squares/Squares.jsx';  // Added the Squares component
+import Squares from './components/Squares/Squares.jsx';  // Importing the Squares component
 
 function App() {
   const slides = [
@@ -16,11 +16,11 @@ function App() {
   ];
 
   return (
-    <div>
+    <div className="App">
       <Squares 
         speed={0.5} 
-        size={40} // pixels
-        direction='diagonal' // up, down, left, right, diagonal
+        size={40} // Size of squares in pixels
+        direction='diagonal' // Movement direction of squares
         borderColor='#fff'
         hoverFillColor='#222'
       />
